24077a302e30ab5d287fb21a76f9751dea6ab140
Aimee Bridges <a.bridges@rgu.ac.uk>
2026-02-25 14:56:55 +0000

    Switching transformation - simplified

--- FiestelCipher.py
+++ FiestelCipher.py
@@ -2,7 +2,7 @@
 
 #Permutation Function
 #Perform IP on 8 bit block using fixed table
-def permutation_function(block: str) -> str:
+def permutation_function(block):
     #Define IP table
     IP = [4, 0, 5, 1, 6, 2, 7, 3]
     #Build permuted block in order specificed by IP
@@ -13,7 +13,7 @@ def permutation_function(block: str) -> str:
 
 #Inverse Permutation Function
 #Perform inverse permutation
-def inverse_permutation_function(block: str) -> str:
+def inverse_permutation_function(block):
     #Define inverse IP table
     inverse_IP = [1, 3, 5, 7, 0, 2, 4, 6]
     #Build inverse permuted block
@@ -24,10 +24,8 @@ def inverse_permutation_function(block: str) -> str:
 
 #Switching Transformation
 #Swaps left and right of 8-bit block
-def switching_transformation(block: str) -> str:
-    #first 4 bits
-    left = block[:4]
-    #last 4 bits
-    right = block[4:]
-    #Return the block with halves swapped
-    transformed_block = right + left
+def switching_transformation(block):
+    #transformed block
+    transformed_block = block[1] + block[0]
+    #return block
+    return transformed_block

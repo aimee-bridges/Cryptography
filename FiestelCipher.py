5c23d488ec1aacb07abd540edc860eb6888a5eb4
Aimee Bridges <a.bridges@rgu.ac.uk>
2026-02-25 14:50:55 +0000

    Initial commit for Fiestel Cipher

---
+++ FiestelCipher.py
@@ -0,0 +1,23 @@
+#Lab 5 = Fiestel Cipher in Python
+
+#Permutation Function
+#Perform IP on 8 bit block using fixed table
+def permutation_function(block: str) -> str:
+    #Define IP table
+    IP = [4, 0, 5, 1, 6, 2, 7, 3]
+    #Build permuted block in order specificed by IP
+    permuted_block = (block[IP[0]] + block[IP[1]] + block[IP[2]] + block[IP[3]] +
+                      block[IP[4]] + block[IP[5]] + block[IP[6]] + block[IP[7]])
+    #return permuted block
+    return permuted_block 
+
+#Inverse Permutation Function
+#Perform inverse permutation
+def inverse_permutation_function(block: str) -> str:
+    #Define inverse IP table
+    inverse_IP = [1, 3, 5, 7, 0, 2, 4, 6]
+    #Build inverse permuted block
+    inverse_permuted_block = (block[inverse_IP[0]] + block[inverse_IP[1]] + block[inverse_IP[2]] + block[inverse_IP[3]] +
+                              block[inverse_IP[4]] + block[inverse_IP[5]] + block[inverse_IP[6]] + block[inverse_IP[7]])
+    #Return inverse permuted block after IP-1
+    return inverse_permuted_block
--- Permuations.py
+++ Permuations.py
@@ -2,10 +2,10 @@ def appl_permutation(bits: str, perm: list[int]) -> str:
     #Apply permutation to a bit-string
     #perm is a list of 1 based indices
 
-    if len(bits != len(perm):
-           raise ValueError("Permutation length must match bit-string length")
-           #Build permuted string by selecting bits in order of perm
-           return "".join(bits[i-1] for i in perm)
+    if len(bits) != len(perm):
+        raise ValueError("Permutation length must match bit-string length")
+    #Build permuted string by selecting bits in order of perm
+    return "".join(bits[i-1] for i in perm)
 
 #Predefined permutations from Lecture 4
 IP = [5, 1, 6, 2, 7, 3, 8, 4]

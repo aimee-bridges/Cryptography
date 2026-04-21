efbc9add54c84cdaa19e0ae18f619208c8ab7bb2
Aimee Bridges <a.bridges@rgu.ac.uk>
2026-04-21 09:53:38 +0100

    Imported modules added

--- Digital_Signature.py
+++ Digital_Signature.py
@@ -1,5 +1,8 @@
 #Imported Modules
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 #Key generation function
-
-#Message signing function

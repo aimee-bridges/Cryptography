b0b83f2d380d1974e78970f35ba42f9c8817a764
Aimee Bridges <a.bridges@rgu.ac.uk>
2026-04-21 10:07:10 +0100

    message sign with private key

--- Digital_Signature.py
+++ Digital_Signature.py
@@ -17,3 +17,16 @@ def generate_key_pair():
     #Get public key
     public_key = private_key.public_key()
     return private_key, public_key
+
+#message sign function
+def sign_message(message, private_key):
+    #use private key to sign message
+    signature = private_key.sign(
+        message.encode('utf-8'),
+        padding.PSS(
+            mgf=padding.mGF1(hashes.SHA2566()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
